8ef10ea6e53e744cfd22c0b08a22cabb0231f00d
Stephen Burnett <stephenburnett@Stephens-MBP.fios-router.home>
2020-04-15 14:22:40 -0400

    Added Components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
+import Header from './Components/Header'
+import Main from './Components/Main'
+import Footer from './Components/Footer'
 import './App.css';
 
 export default function App() {
   return (
     <div className="App">
-      
+      <Header />
+      <Main />
+      <Footer />
     </div>
   );
 }
