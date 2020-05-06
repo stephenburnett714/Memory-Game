ea8bc1b40f457f787a09087e04fec1761325eaaf
Stephen Burnett <stephenburnett@Stephens-MBP.fios-router.home>
2020-05-06 18:00:07 -0400

    able to display a card back from the array

--- src/Components/Game.jsx
+++ src/Components/Game.jsx
@@ -1,22 +1,32 @@
 import React from 'react';
+import CardBack1 from '../Images/CardBacks/CardBack1.png'
+import CardBack2 from '../Images/CardBacks/CardBack2.png'
+import CardBack3 from '../Images/CardBacks/CardBack3.jpg'
 
 export default function Game(props) {
 
+
+// Card Backs 
+  let cardBackArray = [];
+
+  cardBackArray.push(CardBack1) 
+  cardBackArray.push(CardBack2)
+  cardBackArray.push(CardBack3)
+
+// Card Fronts
+let cardFrontArray = []
+
+
+
+
 let numberOfCards = (props.pairs * 2)
-let imgArray =[]
 
 
-function cards(params) {
-  for (var i = 0; i < numberOfCards; i++) {
-    
-    var container = document.getElementById("container");
-    container.innerHTML = '<div class="box"></div>';
- }
-}
 
   return (
     <div>
       {props.pairs}
+      <img src={cardBackArray[2]}></img>
     </div>
   );
 }
