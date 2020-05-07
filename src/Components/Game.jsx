88e22f2e22cb305b6ae0e320a73da344293fcc45
Stephen Burnett <stephenburnett@Stephens-MBP.fios-router.home>
2020-05-07 00:14:04 -0400

    added suffle for array

--- src/Components/Game.jsx
+++ src/Components/Game.jsx
@@ -27,7 +27,7 @@ cardFrontArray.push(CardFront3)
 cardFrontArray.push(CardFront4)
 cardFrontArray.push(CardFront5)
 
-
+// An array of 2 cards per pair
 let cardsUsedInGame = []
 function createCards() {
     for (let i = 0; i < props.pairs; i++) {
@@ -38,11 +38,26 @@ function createCards() {
 createCards()
 
 
+// Suffled Cards
+function shuffleArray(array) {
+  for(let i = array.length - 1; i > 0; i--){
+    const j = Math.floor(Math.random() * i)
+    const temp = array[i]
+    array[i] = array[j]
+    array[j] = temp
+  }
+  return array
+}
+let suffledCards = shuffleArray(cardsUsedInGame)
+
+
+
+
   return (
     <div>
       {props.pairs}
       <img src={cardBack}></img>
-      {cardsUsedInGame.map(card => (
+      {suffledCards.map(card => (
         <img src={card}></img>
       ))}
     </div>
